9b5a87c03c3c11ea1c6ac694af95672f113870e2
Wei ZHANG <wei_zhang_1988@outlook.com>
2024-05-09 10:08:51 +0800

    add primal-dual

--- include/solver.hpp
+++ include/solver.hpp
@@ -340,7 +340,7 @@ class Augmented_Lagrangian_Solver : public Base_Solver{
                               double _epsilon, \
                               std::shared_ptr<Base_Line_Searcher> _sptr_line_searcher, \
                               Eigen::VectorXd _lambdas, \
-                              Eigen::VectorXd _mus, \
+                              Eigen::VectorXd _nvs, \
                               double _sigma = 2.0, \
                               double _enta  = 1e-6, \
                               double _beta1 = 0.3, \
@@ -364,7 +364,7 @@ class Augmented_Lagrangian_Solver : public Base_Solver{
                                           _jac_eq_cons, \
                                           _jac_ieq_cons),  
                               lambdas_(_lambdas),
-                              mus_(_mus),
+                              nvs_(_nvs),
                               sigma_(_sigma),
                               enta_(_enta),
                               beta1_(_beta1),
@@ -383,15 +383,15 @@ class Augmented_Lagrangian_Solver : public Base_Solver{
     epsk_ = std::pow(sigma_, -beta1_);
   }
   double augmented_lagrange(Eigen::VectorXd _var) {
-    Eigen::VectorXd ieq_items_1 = (mus_ / sigma_) + ieq_cons_(_var);
+    Eigen::VectorXd ieq_items_1 = (nvs_ / sigma_) + ieq_cons_(_var);
     ieq_items_1 = ieq_items_1.unaryExpr([](double value) { return (value > 0.0) ? value * value : 0.0; });
-    Eigen::VectorXd ieq_items_2 = (mus_ / sigma_);
+    Eigen::VectorXd ieq_items_2 = (nvs_ / sigma_);
     ieq_items_2 = ieq_items_2.unaryExpr([](double value) { return (value > 0.0) ? value * value : 0.0; });
     Eigen::VectorXd ieq_items = ieq_items_1 - ieq_items_2;
     return obj_fun_(_var) + lambdas_.dot(eq_cons_(_var)) + 0.5 * sigma_ * eq_cons_(_var).squaredNorm() + 0.5 * sigma_ * ieq_items.sum();
   }
   Eigen::VectorXd jac_augmented_lagrange(Eigen::VectorXd _var) {
-    Eigen::VectorXd ieq_rlx_value = -((mus_ / sigma_) + ieq_cons_(_var));
+    Eigen::VectorXd ieq_rlx_value = -((nvs_ / sigma_) + ieq_cons_(_var));
     Eigen::MatrixXd jac_ieq_cons_value = jac_ieq_cons_(_var);
     for (int i = 0; i < ieq_rlx_value.size(); i++) {
       if (ieq_rlx_value(i) < 0.0) {
@@ -401,10 +401,10 @@ class Augmented_Lagrangian_Solver : public Base_Solver{
     ieq_rlx_value = ieq_rlx_value.unaryExpr([](double value) { return (value > 0.0) ? value : 0.0; });
     return (jac_fun_(_var) + \
             (jac_eq_cons_(_var) * (lambdas_ + sigma_ * eq_cons_(_var))) + \
-            jac_ieq_cons_value * (mus_ + sigma_ * (ieq_cons_(_var) + ieq_rlx_value)));
+            jac_ieq_cons_value * (nvs_ + sigma_ * (ieq_cons_(_var) + ieq_rlx_value)));
   }
   Eigen::MatrixXd hes_augmented_lagrange(Eigen::VectorXd _var) {
-    Eigen::VectorXd ieq_rlx_value = -((mus_ / sigma_) + ieq_cons_(_var));
+    Eigen::VectorXd ieq_rlx_value = -((nvs_ / sigma_) + ieq_cons_(_var));
     Eigen::MatrixXd jac_ieq_cons_value = jac_ieq_cons_(_var);
     for (int i = 0; i < ieq_rlx_value.size(); i++) {
       if (ieq_rlx_value(i) < 0.0) {
@@ -421,7 +421,7 @@ class Augmented_Lagrangian_Solver : public Base_Solver{
     sptr_line_searcher_->forward_backward();
   }
   Eigen::VectorXd solve() override {
-    Eigen::VectorXd ieq_rlx_value = -((mus_ / sigma_) + ieq_cons_(var_));
+    Eigen::VectorXd ieq_rlx_value = -((nvs_ / sigma_) + ieq_cons_(var_));
     Eigen::MatrixXd jac_ieq_cons_value = jac_ieq_cons_(var_);
     for (int i = 0; i < ieq_rlx_value.size(); i++) {
       if (ieq_rlx_value(i) < 0.0) {
@@ -433,7 +433,7 @@ class Augmented_Lagrangian_Solver : public Base_Solver{
     do {
       jac_lag_ = (jac_fun_(var_) + \
                   (jac_eq_cons_(var_) * (lambdas_ + sigma_ * eq_cons_(var_))) + \
-                  jac_ieq_cons_value * (mus_ + sigma_ * (ieq_cons_(var_) + ieq_rlx_value)));
+                  jac_ieq_cons_value * (nvs_ + sigma_ * (ieq_cons_(var_) + ieq_rlx_value)));
       Eigen::MatrixXd hes_lag_ = hes_fun_(var_) + jac_eq_cons_(var_) * sigma_ * jac_eq_cons_(var_).transpose()+ jac_ieq_cons_value * sigma_ * jac_ieq_cons_value.transpose();
       dir_ = hes_lag_.template bdcSvd<Eigen::ComputeThinU | Eigen::ComputeThinV>().solve(-jac_lag_);
       sptr_line_searcher_->set_var(var_);
@@ -442,15 +442,15 @@ class Augmented_Lagrangian_Solver : public Base_Solver{
       var_ += alpha_ * dir_;
       iter_num++;
     } while (jac_lag_.norm() > entk_ && std::abs(eq_cons_(var_).norm()) > entk_ && ieq_cons_(var_).maxCoeff() > -std::abs(entk_));
-    cons_violation_ = std::sqrt(eq_cons_(var_).squaredNorm() + (ieq_cons_(var_).cwiseMax(-(mus_ / sigma_))).squaredNorm());
+    cons_violation_ = std::sqrt(eq_cons_(var_).squaredNorm() + (ieq_cons_(var_).cwiseMax(-(nvs_ / sigma_))).squaredNorm());
     if (cons_violation_ < epsk_){
       if (cons_violation_ < epsilon_ && jac_lag_.norm() < enta_) {
         ending_cond_ = true;
         return var_;
       } else {
         lambdas_ += sigma_ * eq_cons_(var_);
-        mus_ += sigma_ * ieq_cons_(var_);
-        mus_ = mus_.unaryExpr([](double value) { return (value > 0.0) ? value : 0.0; });
+        nvs_ += sigma_ * ieq_cons_(var_);
+        nvs_ = nvs_.unaryExpr([](double value) { return (value > 0.0) ? value : 0.0; });
         entk_ /= sigma_;
         epsk_ *= std::pow(sigma_,-beta2_);
       }
@@ -466,7 +466,7 @@ class Augmented_Lagrangian_Solver : public Base_Solver{
     return ending_cond_;
   }
   protected:
-  Eigen::VectorXd jac_lag_, lambdas_, mus_;
+  Eigen::VectorXd jac_lag_, lambdas_, nvs_;
   double sigma_, enta_, epsk_, entk_, beta1_, beta2_, rho_;
   double cons_violation_;
   bool ending_cond_ = false;
@@ -476,7 +476,7 @@ class Log_Barrier_Solver : public Base_Solver{
   Log_Barrier_Solver(Eigen::VectorXd _var, \
                      double _epsilon, \
                      std::shared_ptr<Base_Line_Searcher> _sptr_line_searcher, \
-                     Eigen::VectorXd _lambdas, \
+                     Eigen::VectorXd _nvs, \
                      double _t = 1e-6, \
                      double _mu  = 2.0, \
                      ObjFun _obj_fun = nullptr, \
@@ -496,7 +496,7 @@ class Log_Barrier_Solver : public Base_Solver{
                                  _ieq_cons, \
                                  _jac_eq_cons, \
                                  _jac_ieq_cons),  
-                     lambdas_(_lambdas),
+                     nvs_(_nvs),
                      t_(_t),
                      mu_(_mu){
     assert(t_>0 && mu_>1.0);
@@ -537,28 +537,28 @@ class Log_Barrier_Solver : public Base_Solver{
     sptr_line_searcher_->set_hes_fun(std::bind(&Log_Barrier_Solver::hes_lagrange,this,std::placeholders::_1));
     sptr_line_searcher_->forward_backward();
   }
-  Eigen::VectorXd KKT_rhs(Eigen::VectorXd _var, Eigen::VectorXd _lambdas) {
-    Eigen::VectorXd upper = jac_lagrange(_var) + jac_eq_cons_(_var) * _lambdas;
+  Eigen::VectorXd KKT_rhs(Eigen::VectorXd _var, Eigen::VectorXd _nvs) {
+    Eigen::VectorXd upper = jac_lagrange(_var) + jac_eq_cons_(_var) * _nvs;
     Eigen::VectorXd lower = eq_cons_(_var);
     Eigen::VectorXd ret(upper.size()+lower.size());
     ret<<upper,lower;
     return -ret;
   }
-  Eigen::MatrixXd KKT_matrix(Eigen::VectorXd _var, Eigen::VectorXd _lambdas) {
-    Eigen::MatrixXd matrix = Eigen::MatrixXd::Zero(_var.size()+_lambdas.size(),_var.size()+_lambdas.size());
+  Eigen::MatrixXd KKT_matrix(Eigen::VectorXd _var, Eigen::VectorXd _nvs) {
+    Eigen::MatrixXd matrix = Eigen::MatrixXd::Zero(_var.size()+_nvs.size(),_var.size()+_nvs.size());
     Eigen::MatrixXd hes_eq_values = Eigen::MatrixXd::Zero(_var.size(),_var.size());
-    for (int i = 0; i < _lambdas.size(); i++) {
-      hes_eq_values += _lambdas(i) * hes_eq_cons_(_var)[i];
+    for (int i = 0; i < _nvs.size(); i++) {
+      hes_eq_values += _nvs(i) * hes_eq_cons_(_var)[i];
     }
     matrix.block(0, 0, _var.size(), _var.size()) = hes_lagrange(_var) + hes_eq_values;
-    matrix.block(0, _var.size(), _var.size(), _lambdas.size()) = jac_eq_cons_(_var);
-    matrix.block(_var.size(), 0, _lambdas.size(), _var.size()) = jac_eq_cons_(_var).transpose();
+    matrix.block(0, _var.size(), _var.size(), _nvs.size()) = jac_eq_cons_(_var);
+    matrix.block(_var.size(), 0, _nvs.size(), _var.size()) = jac_eq_cons_(_var).transpose();
     return matrix;
   }
   Eigen::VectorXd solve() override {
     int iter_num=0;
     do {
-      dir_ = KKT_matrix(var_, lambdas_).template bdcSvd<Eigen::ComputeThinU | Eigen::ComputeThinV>().solve(KKT_rhs(var_, lambdas_));
+      dir_ = KKT_matrix(var_, nvs_).template bdcSvd<Eigen::ComputeThinU | Eigen::ComputeThinV>().solve(KKT_rhs(var_, nvs_));
       sptr_line_searcher_->set_var(var_);
       sptr_line_searcher_->set_dir(dir_);
       if (ieq_cons_(var_).any()>=0) {
@@ -567,7 +567,155 @@ class Log_Barrier_Solver : public Base_Solver{
       }
       alpha_ = sptr_line_searcher_->search();
       var_      += alpha_ * dir_.head(var_.size());
-      lambdas_  += alpha_ * dir_.tail(lambdas_.size());
+      nvs_  += alpha_ * dir_.tail(nvs_.size());
+      iter_num++;
+    } while (iter_num < 10);
+    if (nvs_.size() / t_ < epsilon_){
+      ending_cond_ = true;
+    } else{
+      t_ *= mu_;
+    }
+    return var_;
+  }
+  bool ending_condition() {
+    return ending_cond_;
+  }
+  protected:
+  Eigen::VectorXd nvs_;
+  double t_, mu_;
+  bool ending_cond_ = false;
+};
+class Primal_Dual_Solver : public Base_Solver{
+  public:
+  Primal_Dual_Solver(Eigen::VectorXd _var, \
+                     double _epsilon, \
+                     std::shared_ptr<Base_Line_Searcher> _sptr_line_searcher, \
+                     Eigen::VectorXd _lambdas, \
+                     Eigen::VectorXd _nvs, \
+                     double _t = 1e-6, \
+                     double _mu  = 2.0, \
+                     ObjFun _obj_fun = nullptr, \
+                     JacFun _jac_fun = nullptr, \
+                     HesFun _hes_fun = nullptr, \
+                     ConsFun _eq_cons      = nullptr, \
+                     ConsFun _ieq_cons     = nullptr, \
+                     JacCons _jac_eq_cons  = nullptr, \
+                     JacCons _jac_ieq_cons = nullptr) :
+                     Base_Solver(_var, \
+                                 _epsilon, \
+                                 _sptr_line_searcher, \
+                                 _obj_fun, \
+                                 _jac_fun, \
+                                 _hes_fun, \
+                                 _eq_cons, \
+                                 _ieq_cons, \
+                                 _jac_eq_cons, \
+                                 _jac_ieq_cons), 
+                     lambdas_(_lambdas), 
+                     nvs_(_nvs),
+                     t_(_t),
+                     mu_(_mu){
+    assert(t_>0 && mu_>1.0);
+  }
+  double lagrange(Eigen::VectorXd _var_lambdas_nvs) {
+    Eigen::VectorXd _var     = _var_lambdas_nvs.head(var_.size());
+    Eigen::VectorXd _lambdas = _var_lambdas_nvs.segment(var_.size(),lambdas_.size());
+    Eigen::VectorXd _nvs     = _var_lambdas_nvs.tail(nvs_.size());
+    return obj_fun_(_var) + _lambdas.dot(ieq_cons_(_var)) + _nvs.dot(eq_cons_(_var));
+  }
+  Eigen::VectorXd jac_lagrange(Eigen::VectorXd _var_lambdas_nvs) {
+    Eigen::VectorXd _var     = _var_lambdas_nvs.head(var_.size());
+    Eigen::VectorXd _lambdas = _var_lambdas_nvs.segment(var_.size(),lambdas_.size());
+    Eigen::VectorXd _nvs     = _var_lambdas_nvs.tail(nvs_.size());
+    Eigen::VectorXd jac_lagrange_values(_var_lambdas_nvs.size());
+    Eigen::MatrixXd jac_ieq_cons_values = jac_ieq_cons_(_var);
+    Eigen::VectorXd jac_ieq_cons_items = Eigen::VectorXd::Zero(_var.size());
+    for (int i = 0; i < _lambdas.size(); i++) {
+      jac_ieq_cons_items += jac_ieq_cons_values.col(i) * _lambdas(i);
+    }
+    Eigen::MatrixXd jac_eq_cons_values = jac_eq_cons_(_var);
+    Eigen::VectorXd jac_eq_cons_items = Eigen::VectorXd::Zero(_var.size());
+    for (int i = 0; i < _nvs.size(); i++) {
+      jac_eq_cons_items += jac_eq_cons_values.col(i) * _nvs(i);
+    }
+    jac_lagrange_values<<jac_fun_(_var) + jac_ieq_cons_items +  jac_eq_cons_items, \
+                         ieq_cons_(_var), \
+                         eq_cons_(_var);
+    return jac_lagrange_values;
+  }
+  Eigen::MatrixXd hes_lagrange(Eigen::VectorXd _var_lambdas_nvs) {
+    Eigen::VectorXd _var     = _var_lambdas_nvs.head(var_.size());
+    Eigen::VectorXd _lambdas = _var_lambdas_nvs.segment(var_.size(),lambdas_.size());
+    Eigen::VectorXd _nvs     = _var_lambdas_nvs.tail(nvs_.size());
+    Eigen::MatrixXd hes_lagrange_values(_var_lambdas_nvs.size(),_var_lambdas_nvs.size());
+    std::vector<Eigen::MatrixXd> hes_ieq_cons_values = hes_ieq_cons_(_var);
+    Eigen::MatrixXd hes_ieq_cons_items = Eigen::MatrixXd::Zero(_lambdas.size(),_lambdas.size());
+    for (int i = 0; i < _lambdas.size(); i++) {
+      hes_ieq_cons_items += hes_ieq_cons_values[i] * _lambdas(i);
+    }
+    std::vector<Eigen::MatrixXd> hes_eq_cons_values = hes_eq_cons_(_var);
+    Eigen::MatrixXd hes_eq_cons_items = Eigen::MatrixXd::Zero(_nvs.size(),_nvs.size());
+    for (int i = 0; i < _nvs.size(); i++) {
+      hes_eq_cons_items += hes_eq_cons_values[i] * _nvs(i);
+    }
+    hes_lagrange_values.block(0, 0, _var.size(), _var.size()) = hes_fun_(_var) + hes_ieq_cons_items + hes_eq_cons_items;
+    hes_lagrange_values.block(0, _var.size(), _var.size(), _lambdas.size()) = jac_ieq_cons_(_var).transpose();
+    hes_lagrange_values.block(0, _var.size()+_lambdas.size(), _var.size(), _nvs.size()) = jac_eq_cons_(_var).transpose();
+    hes_lagrange_values.block(_var.size(), 0, _lambdas.size(), _var.size()) = jac_ieq_cons_(_var);
+    hes_lagrange_values.block(_var.size()+_lambdas.size(), 0, _nvs.size(), _var.size()) = jac_eq_cons_(_var);
+    return hes_lagrange_values;
+  }
+  virtual void init_for_solve() {
+    sptr_line_searcher_->set_obj_fun(std::bind(&Primal_Dual_Solver::lagrange,this,std::placeholders::_1));
+    sptr_line_searcher_->set_jac_fun(std::bind(&Primal_Dual_Solver::jac_lagrange,this,std::placeholders::_1));
+    sptr_line_searcher_->set_hes_fun(std::bind(&Primal_Dual_Solver::hes_lagrange,this,std::placeholders::_1));
+    sptr_line_searcher_->forward_backward();
+  }
+  Eigen::VectorXd KKT_rhs(Eigen::VectorXd _var, Eigen::VectorXd _lambdas, Eigen::VectorXd _nvs) {
+    Eigen::VectorXd var_lambdas_nvs(_var.size() + _lambdas.size() + _nvs.size());
+    var_lambdas_nvs<<_var, _lambdas, _nvs;
+    Eigen::VectorXd upper  = jac_lagrange(var_lambdas_nvs);
+    Eigen::VectorXd middle = _lambdas.asDiagonal() * ieq_cons_(_var) + 1/t_ * Eigen::MatrixXd::Ones(ieq_cons_(_var).size());
+    Eigen::VectorXd lower  = eq_cons_(_var);
+    Eigen::VectorXd ret(var_lambdas_nvs.size());
+    ret<<upper, middle, lower;
+    return -ret;
+  }
+  Eigen::MatrixXd KKT_matrix(Eigen::VectorXd _var, Eigen::VectorXd _lambdas, Eigen::VectorXd _nvs) {
+    Eigen::VectorXd var_lambdas_nvs(_var.size() + _lambdas.size() + _nvs.size());
+    Eigen::MatrixXd matrix = Eigen::MatrixXd::Zero(var_lambdas_nvs.size(),var_lambdas_nvs.size());
+    std::vector<Eigen::MatrixXd> hes_ieq_cons_values = hes_ieq_cons_(_var);
+    Eigen::MatrixXd hes_ieq_cons_items = Eigen::MatrixXd::Zero(_lambdas.size(),_lambdas.size());
+    for (int i = 0; i < _lambdas.size(); i++) {
+      hes_ieq_cons_items += hes_ieq_cons_values[i] * _lambdas(i);
+    }
+    std::vector<Eigen::MatrixXd> hes_eq_cons_values = hes_eq_cons_(_var);
+    Eigen::MatrixXd hes_eq_cons_items = Eigen::MatrixXd::Zero(_nvs.size(),_nvs.size());
+    for (int i = 0; i < _nvs.size(); i++) {
+      hes_eq_cons_items += hes_eq_cons_values[i] * _nvs(i);
+    }
+    matrix.block(0, 0, _var.size(), _var.size()) = hes_fun_(_var) + hes_ieq_cons_items + hes_eq_cons_items;
+    matrix.block(0, _var.size(), _var.size(), _lambdas.size()) = jac_ieq_cons_(_var).transpose();
+    matrix.block(0, _var.size()+_lambdas.size(), _var.size(), _nvs.size()) = jac_eq_cons_(_var).transpose();
+    matrix.block(_var.size(), 0, _lambdas.size(), _var.size()) = _lambdas.asDiagonal() * jac_ieq_cons_(_var);
+    matrix.block(_var.size(), _var.size(), _lambdas.size(), _lambdas.size()) = ieq_cons_(_var).asDiagonal();
+    matrix.block(_var.size()+_lambdas.size(), 0, _nvs.size(), _var.size()) = jac_eq_cons_(_var);
+    return matrix;
+  }
+  Eigen::VectorXd solve() override {
+    Eigen::VectorXd var_lambdas_nvs(var_.size() + lambdas_.size() + nvs_.size());
+    var_lambdas_nvs<<var_, lambdas_, nvs_;
+    int iter_num=0;
+    do {
+      dir_ = KKT_matrix(var_, lambdas_, nvs_).template bdcSvd<Eigen::ComputeThinU | Eigen::ComputeThinV>().solve(KKT_rhs(var_, lambdas_, nvs_));
+      sptr_line_searcher_->set_var(var_lambdas_nvs);
+      sptr_line_searcher_->set_dir(dir_);
+      sptr_line_searcher_->search();
+      alpha_ = sptr_line_searcher_->search();
+      var_lambdas_nvs  += alpha_ * dir_;
+      var_     = var_lambdas_nvs.head(var_.size());
+      lambdas_ = var_lambdas_nvs.segment(var_.size(), lambdas_.size());
+      nvs_     = var_lambdas_nvs.tail(nvs_.size());
       iter_num++;
     } while (iter_num < 10);
     t_ *= mu_;
@@ -577,7 +725,7 @@ class Log_Barrier_Solver : public Base_Solver{
     return ending_cond_;
   }
   protected:
-  Eigen::VectorXd lambdas_;
+  Eigen::VectorXd lambdas_, nvs_;
   double t_, mu_;
   bool ending_cond_ = false;
 };
